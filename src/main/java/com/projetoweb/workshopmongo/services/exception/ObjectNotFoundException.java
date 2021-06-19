a4e72a4b73f401d680c823c2d05e10a2d06eab09
georghiton-luiz <georghiton@hotmail.com>
2021-06-19 13:40:27 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/projetoweb/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.projetoweb.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
